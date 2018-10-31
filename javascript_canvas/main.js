2c1911de09ad5653f6167e355e3420108f42d770
t-osawa <retas_seven@hotmail.co.jp>
2018-10-31 23:01:32 +0900

    タイマー処理を追加

--- javascript_canvas/main.js
+++ javascript_canvas/main.js
@@ -2,12 +2,18 @@
 const WIDTH = 640;
 /** 画面高さ */
 const HEIGHT = 480;
+/** １フレームの描画に使用する時間（ミリ秒） */
+const FRAME_MSEC = 1000 / 45;
 /** コンテキスト（前面CANVAS） */
 var front;
 /** コンテキスト（背面CANVAS） */
 var back;
 /** 画面（各場面） */
 var state;
+/** 描画タイミング調整用の変数：ループ開始時の時間（ミリ秒） */
+var loopStartMsc;
+/** 描画タイミング調整用の変数：ループ終了時の時間（ミリ秒） */
+var loopEndMsc;
 
 /**
  * 初期処理
@@ -31,19 +37,25 @@ function init() {
  * メインループ
  */
 function mainLoop() {
-    // 画面を初期化
-    // front.fillStyle = 'rgb(0, 0, 0)';
-    // front.fillRect(0, 0, WIDTH, HEIGHT);
-    front.clearRect(0, 0, WIDTH, HEIGHT);
-    // 描画
-    state.draw();
-    // 状態を更新
-    state.run();
+    loopStartMsc = Date.now();
+    
+    if (FRAME_MSEC < (loopStartMsc - loopEndMsc)) {
+        // 画面を初期化
+        front.clearRect(0, 0, WIDTH, HEIGHT);
+        // 描画
+        state.draw();
+        // 状態を更新
+        state.run();
 
+        // 描画間隔調整のための処理
+        loopEndMsc = loopStartMsc;
+    }
+    
     requestAnimationFrame(mainLoop);
 }
 
 // 画面を初期化
 init();
 // メインループ開始
+loopEndMsc = 0;
 requestAnimationFrame(mainLoop);
